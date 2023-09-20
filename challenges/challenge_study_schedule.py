22ed8acfa01e698c197781581dc9caa0d945f6de
Victoria Sales <contatovictoriasales@gmail.com>
2023-09-20 16:35:27 -0300

    Implementa função study_schedule (Req.01)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    count = 0
+
+    for start_time, end_time in permanence_period:
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
+        if start_time <= target_time and end_time >= target_time:
+            count += 1
+
+    return count
