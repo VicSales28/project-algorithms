5b3d291c7c118827abec4380982f1cc81c4c2f8d
Victoria Sales <contatovictoriasales@gmail.com>
2023-09-20 16:41:24 -0300

    Implementa teste em test_encrypt.py (Req.02)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("abbcca", "4")
+
+    with pytest.raises(TypeError):
+        encrypt_message(112233, 3)
+
+    assert encrypt_message("aabbcc", 3) == "baa_ccb"
+    assert encrypt_message("abbcca", 4) == "ac_cbba"
+    assert encrypt_message("word", 9) == "drow"
