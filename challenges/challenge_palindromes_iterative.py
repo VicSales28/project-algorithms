f2508e22192957a8b446e143677fb47d371f4c31
Victoria Sales <contatovictoriasales@gmail.com>
2023-09-21 13:29:51 -0300

    Corrige erros (Flake8)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,11 @@
 def is_palindrome_iterative(word):
-    if not word or not isinstance(word,str):
+    if not word or not isinstance(word, str):
         return False
-    
+
     word = word.lower()
 
     left, right = 0, len(word) - 1
-    
+
     while left < right:
         if word[left] != word[right]:
             return False
