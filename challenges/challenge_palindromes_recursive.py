95875fe3a7bc2a238477113d1403d6fbc6dd2179
Victoria Sales <contatovictoriasales@gmail.com>
2023-09-21 08:12:28 -0300

    Implementa função is_palindrome_recursive (Req.03)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or len(word) == 0:
+        return False
+
+    word = word.lower()
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
